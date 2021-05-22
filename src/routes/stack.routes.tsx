a9c35ad841795ba1b69c914c11c208d0609d4d4e
Guilherme Andrade Lacerda <guilherme.lacerda@itpac_arn.local>
2021-05-22 06:25:10 -0300

    create componente register and complete

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import colors from "../styles/colors";
+import { Welcome } from "../pages/Welcome";
+import { Register } from "../pages/Register";
+import { CompleteRegister } from "../pages/CompleteRegister";
+
+const StackRoutes = createStackNavigator();
+
+export default function AppRoutes() {
+  return (
+    <StackRoutes.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: colors.white,
+        },
+      }}
+    >
+      <StackRoutes.Screen name="Welcome" component={Welcome} />
+      <StackRoutes.Screen name="Register" component={Register} />
+      <StackRoutes.Screen
+        name="CompleteRegister"
+        component={CompleteRegister}
+      />
+    </StackRoutes.Navigator>
+  );
+}
---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+export default {
+    primary: '#4285F4',
+    title: '#1C4C9A',
+    text: '#414141',
+    textInput: '#9D9D9D',
+    shape: '#EEEEEE',
+    white: "#FFF",
+    gray: '#CFCFCF',
+    base: '#757575',
+    red: '#e83f5b',
+    background: '#E8F1FF'
+};
